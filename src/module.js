462788a83cb9e6ea3a75ad8df458fd238d295a39
Michael Filbin <michael.filbin@gmail.com>
2014-01-05 12:39:10 -0700

    initial commit

--- src/module.js
+++ src/module.js
@@ -1,26 +1,15 @@
-(function (angular) {
-    "use strict";
-
-    angular.module('az.config', []).value('az.config', {
-        defaults: {
-            CENTER      : '38,-99',
-            ZOOM        : 5,
-            CRS         : '3857',
-            DISP_CRS    : '4326',
-            SRS         : 'EPSG:4326',
-            OL_CONTROLS : 'zoom,navigation,attribution',
-            MARKER      : {
-                ICON: 'http://www.openlayers.org/dev/img/marker.png',
-                DIMS: {H: 25, W: 21}
-            },
-            OL_CTRL_OPTS: {},
-            TILE_URL    : "http://otile${s}.mqcdn.com/tiles/1.0.0/map/${z}/${x}/${y}.png",
-            SUBDOMAINS  : [1, 2, 3, 4]
-        }
-    });
-    angular.module('az.services', ['az.config']);
-    angular.module('az.directives', ['az.services', 'az.config']);
-    angular.module('az', ['az.services', 'az.directives', 'az.config']);
-})(window.angular);
-
-
+angular.module('az.config', []).value('az.config', {
+	defaults:{
+		CENTER: '38,-99',
+		ZOOM: 5,
+		CRS: '3857',
+		DISP_CRS:'4326',
+		OL_CONTROLS:'zoom,navigation,attribution',
+		OL_CTRL_OPTS:{},
+		TILE_URL:"http://otile${s}.mqcdn.com/tiles/1.0.0/map/${z}/${x}/${y}.png",
+		SUBDOMAINS:[1,2,3,4]
+	}
+});
+angular.module('az.services',['az.config']);
+angular.module('az.directives',['az.services', 'az.config']);
+angular.module('az', ['az.services', 'az.directives', 'az.config']);
